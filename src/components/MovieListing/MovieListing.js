97eba5315d3e3711c3abfc2c9becb5ccc286adf2
Ritesh Hansalia <riteshansalia5@gmail.com>
2024-03-03 11:39:16 +0530

    show the movie list

--- src/components/MovieListing/MovieListing.js
+++ src/components/MovieListing/MovieListing.js
@@ -1,5 +1,32 @@
+import { useSelector } from "react-redux";
+import { getAllMovies } from "../../redux/movies/movieSlice";
+import MovieCard from "../MovieCard/MovieCard";
+import "./MovieListing.scss";
+
 const MovieListing = () => {
-  return <div>MovieListing</div>;
+  const movies = useSelector(getAllMovies);
+
+  let renderMovies = "";
+
+  renderMovies =
+    movies.Response === "True" ? (
+      movies.Search.map((movie, index) => (
+        <MovieCard key={index} data={movie} />
+      ))
+    ) : (
+      <div className="movies-error">
+        <h3>{movies.Error}</h3>
+      </div>
+    );
+
+  return (
+    <div className="movie-warpper">
+      <div className="movie-list">
+        <h2>Movie</h2>
+        <div className="movie-container">{renderMovies}</div>
+      </div>
+    </div>
+  );
 };
 
 export default MovieListing;
